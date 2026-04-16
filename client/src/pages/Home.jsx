0fd4e3c2fdb7c281a27e410bead6f686f58e09a3
Jacob Lyon <jacoblyon31415@gmail.com>
2026-04-15 19:42:20 -0600

    Added content to home page. Includes button to encrypt or login depending on if the user is logged in

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useRequireUser } from "../hooks/useRequireUser";
 import logo from "../assets/logo.png";
 import { useAuth } from "../hooks/useAuth";
+import { Link } from "react-router";
 
 function Home() {
   const {user} = useAuth();
@@ -8,7 +9,12 @@ function Home() {
   return (
     <div style={{ textAlign: "center" }}>
       {user ? <h2>Welcome, {user.username}!</h2> : <h2>Welcome to WebEncrypt!</h2>}
-      <img src={logo} alt="My App" className="home-logo" />
+      <div>
+        This is a website where you can encrypt and decrypt messages using various ciphers.
+        You can save your ciphertext on the ciphers page. There you can also temporaily decrypt your message to read its plaintext.
+        Don't worry! Your plaintext is never stored, so you don't have to worry about your messages being leaked.
+      </div>
+      {user ? <Link to="/encrypt">Encrypt</Link> : <Link to="/login">Login to Encrypt</Link>}
     </div>
   );
 }

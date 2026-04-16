8e927fc4f519d21736ca3dc96a964fb3b6e45c61
Jacob Lyon <jacoblyon31415@gmail.com>
2026-04-15 19:48:21 -0600

    Added content (but not functionality) to the encryption page

--- client/src/pages/Encrypt.jsx
+++ client/src/pages/Encrypt.jsx
@@ -1,10 +1,35 @@
 import { useRequireUser } from "../hooks/useRequireUser";
+import { useState } from "react";
 
 function Encrypt() {
     const user = useRequireUser();
+    const [message, setMessage] = useState("");
+    const [algorithm, setAlgorithm] = useState("caesar");
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+    }
 
     return (
-        <div>This is the encryption page</div>
+        <div>
+            <h2>Welcome to the Encryption Page</h2>
+            <div>
+                Type your message here and select which encryption algorithm you want to use.
+                Submitting your message with encrypt it with the chosen algorithm and save it to the server.
+                Only you will be able to see and decrypt your messages.
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <label htmlFor="algorithm">Algorithm:</label>
+                <select id="algorithm" name="algorithm" value={algorithm} onChange={(e) => setAlgorithm(e.target.value)}>
+                    <option value="caesar">Caesar Cipher</option>
+                    <option value="hill">Hill Cipher</option>
+                </select>
+                <button type="submit">Encrypt</button>
+            </form>
+        </div>
     )
 }
 

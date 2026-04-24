7833a3af3addd7c0f5c94c19ef8c4a4e474c591f
Jacob Lyon <jacoblyon31415@gmail.com>
2026-04-24 15:41:35 -0600

    Updated database so tables are better

--- server/models/ciphers.js
+++ server/models/ciphers.js
@@ -5,10 +5,10 @@ function isAuthorized(userId, cipherUserId) {
     return userId === cipherUserId;
 }
 
-export async function createCipher(userId, ciphertext, algorithm, key) {
+export async function createCipher(userId, algorithm, cipherData) {
     const result = await pool.query(
-        "INSERT INTO ciphers (user_id, ciphertext, algorithm, key) VALUES ($1, $2, $3, $4) RETURNING *",
-        [userId, ciphertext, algorithm, key]);
+        "INSERT INTO ciphers (user_id, algorithm, cipher_data) VALUES ($1, $2, $3) RETURNING *",
+        [userId, algorithm, cipherData]);
     return result.rows[0];
 }
 
